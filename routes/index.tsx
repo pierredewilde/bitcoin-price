187a8d9dc818b3dd62958e9547051a25a46df412
Pierre De Wilde <pierredewilde@gmail.com>
2022-07-07 23:41:19 +0200

    Fix last refresh date

--- routes/index.tsx
+++ routes/index.tsx
@@ -84,7 +84,7 @@ export default function Home({ data }: PageProps<Price | null>) {
           EUR: € {data.bpi.EUR.rate}
         </p>
         <p class={tw`my-5 text(center sm white)`}>
-          Last refresh at {new Date(data.time.updated).toString()}
+          Last refresh at {new Date().toString()}
         </p>
         <p class={tw`text(center sm white)`}>
           <Refresh target={date.toISOString()}/>
